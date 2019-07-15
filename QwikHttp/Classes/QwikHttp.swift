19abb3b9de0e263c5ecfeaf0227fcb7f90197e9e
Logan Sease <logan@team.casa>
2019-07-15 08:51:36 -0400

    reset before resend

--- QwikHttp/Classes/QwikHttp.swift
+++ QwikHttp/Classes/QwikHttp.swift
@@ -650,6 +650,7 @@ extension QwikHttp
     //this method is primarily used for the response interceptor as any easy way to restart the request
     @objc public func resend(_ handler: @escaping (Data?,URLResponse?, NSError? ) -> Void)
     {
+        self.reset()
         HttpRequestPooler.sendRequest(self, handler: handler)
     }
     
